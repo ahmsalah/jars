fea2c948ab93cf03f4166a82209375ccef63a46f
ahmsalah <50025978+ahmsalah@users.noreply.github.com>
2019-10-01 23:14:16 +0200

    clean up code, remove comments

--- src/App.js
+++ src/App.js
@@ -43,15 +43,13 @@ function App() {
 	);
 
 	const onSortEnd = ({ oldIndex, newIndex, collection }) => {
-		if (collection === 'exp') {
-			setExpCategories(expCategories =>
-				arrayMove(expCategories, oldIndex, newIndex)
-			);
-		} else if (collection === 'inc') {
-			setIncCategories(incCategories =>
-				arrayMove(incCategories, oldIndex, newIndex)
-			);
-		}
+		collection === 'exp'
+			? setExpCategories(expCategories =>
+					arrayMove(expCategories, oldIndex, newIndex)
+				)
+			: setIncCategories(incCategories =>
+					arrayMove(incCategories, oldIndex, newIndex)
+				);
 	};
 
 	const removeCategory = id => {
@@ -97,88 +95,3 @@ function App() {
 }
 
 export default App;
-
-// class App extends Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			allCategories: [
-// 				{ id: uuid(), name: 'Groceries', type: 'exp' },
-// 				{ id: uuid(), name: 'Internet', type: 'exp' },
-// 				{ id: uuid(), name: 'Taxi', type: 'exp' },
-// 				{ id: uuid(), name: 'Salary', type: 'inc' },
-// 				{ id: uuid(), name: 'Savings', type: 'inc' },
-// 				{ id: uuid(), name: 'Bonus', type: 'inc' }
-// 			],
-// 			expCategories: [],
-// 			incCategories: []
-// 		};
-
-// 	}
-
-// 	componentDidMount() {
-// 		this.splitCategories();
-// 	}
-
-// 	onSortEnd = ({ oldIndex, newIndex, collection }) => {
-// 		if (collection === 'exp') {
-// 			this.setState(({ expCategories }) => ({
-// 				expCategories: arrayMove(expCategories, oldIndex, newIndex)
-// 			}));
-// 		} else if (collection === 'inc') {
-// 			this.setState(({ incCategories }) => ({
-// 				incCategories: arrayMove(incCategories, oldIndex, newIndex)
-// 			}));
-// 		}
-// 	};
-
-// 	// removeCategory = id => {
-// 	// 	this.setState({ allCategories: this.state.allCategories.filter(ct => ct.id !== id) }, () =>
-// 	// 		this.splitCategories()
-// 	// 	);
-// 	// };
-
-// 	// splitCategories = () => {
-// 	// 	let [ incArray, expArray ] = pushToArrays(this.state.allCategories);
-// 	// 	this.setState({ expCategories: expArray, incCategories: incArray });
-// 	// };
-
-// 	// addCategory = newCategory => {
-// 	// 	this.setState({ allCategories: [ ...this.state.allCategories, newCategory ] }, () =>
-// 	// 		this.splitCategories()
-// 	// 	);
-// 	// };
-
-// 	render() {
-// 		const { expCategories, incCategories } = this.state;
-
-// 		return (
-// 			<div className="App">
-// 				<Sidebar />
-// 				<Switch>
-// 					<Route
-// 						exact
-// 						path="/"
-// 						render={() => <Home expCategories={expCategories} incCategories={incCategories} />}
-// 					/>
-// 					<Route
-// 						exact
-// 						path="/categories"
-// 						render={() => (
-// 							<Categories
-// 								expCategories={expCategories}
-// 								incCategories={incCategories}
-// 								addCategory={this.addCategory}
-// 								removeCategory={this.removeCategory}
-// 								onSortEnd={this.onSortEnd}
-// 							/>
-// 						)}
-// 					/>
-// 					<Redirect to="/" />
-// 				</Switch>
-// 			</div>
-// 		);
-// 	}
-// }
-
-// export default App;
